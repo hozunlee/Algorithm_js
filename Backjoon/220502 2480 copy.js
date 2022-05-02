2a7859659c0db4131961b3a4c894b7ddbafc3e70
ho2yahh <ho2yahh@gmail.com>
2022-05-02 13:51:18 +0900

    [BJ] 2480, 2525

---
+++ Backjoon/220502 2480 copy.js
@@ -0,0 +1,25 @@
+// https://www.acmicpc.net/problem/2480
+
+const input = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .split(" ")
+    .map((value) => +value);
+
+input.sort(function (a, b) {
+    return b - a;
+});
+
+const [A, B, C] = input;
+
+if (A !== B && A !== C && B !== C) {
+    console.log(A * 100);
+} else if (A === B && B !== C) {
+    console.log(1000 + A * 100);
+} else if (B === C && C !== A) {
+    console.log(1000 + C * 100);
+} else if (C === A && A !== B) {
+    console.log(1000 + C * 100);
+} else {
+    console.log(10000 + A * 1000);
+}
---
+++ Backjoon/220502 25083.js
@@ -0,0 +1,8 @@
+console.log(`         ,r'"7`);
+console.log("r`-_   ,'  ,/");
+console.log(` \\. ". L_r'`);
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
+
+// 출력이 안되는 \ 앞에 \\로 만듦으로서 출력되게 함
