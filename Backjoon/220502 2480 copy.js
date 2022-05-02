fd1730e53a91a7f9fd20d2dfe8a738618f7a9d26
ho2yahh <ho2yahh@gmail.com>
2022-05-02 13:53:35 +0900

    2480

--- Backjoon/220502 2480 copy.js
+++ Backjoon/220502 2480 copy.js
@@ -23,3 +23,21 @@ if (A !== B && A !== C && B !== C) {
 } else {
     console.log(10000 + A * 1000);
 }
+
+//숏코딩
+
+let [a, b, c] = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split(" ")
+    .map(Number)
+    .sort((x, y) => x - y);
+
+if (a == b && b == c) {
+    console.log(10000 + a * 1000);
+} else if (a == b || b == c) {
+    console.log(1000 + b * 100);
+} else {
+    console.log(c * 100);
+}
