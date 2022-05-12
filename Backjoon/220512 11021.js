1534b52a4c18fdacd639f70936ce43703e46c01a
ho2yahh <ho2yahh@gmail.com>
2022-05-12 14:03:37 +0900

    [bj] 11021, 11022

---
+++ Backjoon/220512 11021.js
@@ -0,0 +1,8 @@
+//https://www.acmicpc.net/problem/11021
+
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+for (i = 1; i <= input[0]; i++) {
+    let [a, b] = input[i].split(" ").map((value) => +value);
+    console.log(`Case #${i}: ${a + b}`);
+}
